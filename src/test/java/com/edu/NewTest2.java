503cc888941c607f14044947a1354a88e0ab568a
sidmudda <68602356+sidmudda@users.noreply.github.com>
2020-10-19 17:35:49 +0530

    Update NewTest2.java

--- src/test/java/com/edu/NewTest2.java
+++ src/test/java/com/edu/NewTest2.java
@@ -16,7 +16,7 @@ public class NewTest2 {
   @BeforeMethod
   public void launch() {
 	  
-	    System.setProperty("webdriver.chrome.driver", "chromedriver");
+	    System.setProperty("webdriver.chrome.driver", "/home/ubuntu/chromedriver");
 	    driver = new ChromeDriver();
 	    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	    driver.get("http://35.190.172.87:8081/");
