f0f53637ee044e26da855d87b05053017ba5de8d
sidmudda <68602356+sidmudda@users.noreply.github.com>
2020-10-19 19:44:02 +0530

    Update NewTest1.java

--- src/test/java/com/edu/NewTest1.java
+++ src/test/java/com/edu/NewTest1.java
@@ -18,7 +18,7 @@ public class NewTest1 {
   @BeforeMethod
   public void beforeMethod() {
 	  
-	    System.setProperty("webdriver.chrome.driver", "/home/jslave/chromedriver");
+	    System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 	    driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.get("http://35.190.172.87:8081/");
